890d1937b4636b1d2fcfeabb47f1e64cf25d3ec8
Talyta Scaramuzzo <64484600+ll-talyta-ll@users.noreply.github.com>
2023-11-05 11:16:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,117 +1,22 @@
 import pygame
 import random
 
-# Inicialização do Pygame
-pygame.init()
-
-# Configurações da tela
-largura_tela = 800
-altura_tela = 600
-tela = pygame.display.set_mode((largura_tela, altura_tela))
-pygame.display.set_caption("Jogo de Subir Obstáculos")
-
-# Cores
-branco = (255, 255, 255)
-
-# Personagem
-player_image = pygame.image.load('player.png')
-player_x = largura_tela // 2 - 25
-player_y = altura_tela - 100
-player_velocidade = 5
-
-# Obstáculos
-obstaculos = []
-obstaculo_image = pygame.image.load('obstaculo.png')
-obstaculo_velocidade = 3
-obstaculo_respawn_timer = 100
-
-# Sons
-pygame.mixer.music.load('musica_fundo.mp3')
-pygame.mixer.music.play(-1)
-som_colisao = pygame.mixer.Sound('som_colisao.wav')
-
-# Pontuação
-pontuacao = 0
-fonte = pygame.font.Font(None, 36)
-
-# Função para desenhar a pontuação na tela
-def mostrar_pontuacao():
-    texto = fonte.render(f'Pontuação: {pontuacao}', True, branco)
-    tela.blit(texto, (10, 10))
-
-# Loop do jogo
-jogando = True
-while jogando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            jogando = False
-
-    # Movimentação do jogador
-    teclas = pygame.key.get_pressed()
-    if teclas[pygame.K_LEFT] and player_x > 0:
-        player_x -= player_velocidade
-    if teclas[pygame.K_RIGHT] and player_x < largura_tela - 50:
-        player_x += player_velocidade
-
-    # Movimentação dos obstáculos
-    for obstaculo in obstaculos:
-        obstaculo[1] += obstaculo_velocidade
-        if obstaculo[1] > altura_tela:
-            obstaculos.remove(obstaculo)
-            pontuacao += 1
-
-    # Respawn de obstáculos
-    obstaculo_respawn_timer -= 1
-    if obstaculo_respawn_timer == 0:
-        obstaculos.append([random.randint(0, largura_tela - 50), 0])
-        obstaculo_respawn_timer = 100
-
-    # Verificação de colisão com obstáculos
-    for obstaculo in obstaculos:
-        if (player_x < obstaculo[0] < player_x + 50 or player_x < obstaculo[0] + 50 < player_x + 50) and player_y < obstaculo[1] + 50:
-            som_colisao.play()
-            jogando = False
-
-    # Desenhar na tela
-    tela.fill((0, 0, 0))
-    tela.blit(player_image, (player_x, player_y))
-    mostrar_pontuacao()
-    for obstaculo in obstaculos:
-        tela.blit(obstaculo_image, (obstaculo[0], obstaculo[1]))
-
-    pygame.display.update()
-
-# Tela de Game Over
-tela.fill((0, 0, 0))
-texto_gameover = fonte.render("Game Over", True, branco)
-tela.blit(texto_gameover, (largura_tela // 2 - 100, altura_tela // 2 - 50))
-mostrar_pontuacao()
-pygame.display.update()
-
-
-
-# IMPLEMENTAÇÃO
-
-import pygame
-import random
-
 largura = 1000
-altura = 700
+largura_obstaculo = 2000
+altura_obstaculo = 500
+altura = 1200
 frames = 60
-cor_branca = (255, 255, 255)
-cor_vermelho = (255, 0, 0)
-cor_prata = (192, 192, 192)
-cor_amarelo = (255, 255, 0)
-cor_verde = (0, 255, 0)
-solo = 250
-velocidade_mapa = 10
+#posição do solo
+solo = 320
+#Campo utilizado para manipular a velocidade do cenário
+velocidade_mapa = 2
 
-
+#Manipulação do obstaculo na base inferior da tela
 class Cano(pygame.sprite.Sprite):
     def __init__(self, imagem):
         self.imagem = imagem
         self.rect = self.imagem.get_rect()
-        self.rect.top, self.rect.left = 320, largura + random.randint(0, 1000)
+        self.rect.top, self.rect.left = 270, largura_obstaculo + random.randint(0, 1000)
 
     def update(self, superficie):
         superficie.blit(self.imagem, self.rect)
@@ -120,8 +25,8 @@ class Cano(pygame.sprite.Sprite):
         self.rect.move_ip(-velocidade_mapa, 0)
 
     def recriar(self):
-        if self.rect.left < -200:
-            self.rect.top, self.rect.left = 320, largura + random.randint(0, 2000)
+        if self.rect.left < -400:
+            self.rect.top, self.rect.left = 270, largura_obstaculo + random.randint(0, 2000)
 
 
 class Player(pygame.sprite.Sprite):
@@ -162,10 +67,10 @@ def main(frames):
 
     img_fundo1 = pygame.image.load('fundo.png').convert_alpha()
     img_fundo2 = pygame.image.load('fundo.png').convert_alpha()
-    cano = pygame.image.load('cano.png').convert_alpha()
+    cano = pygame.image.load('Water.png').convert_alpha()
 
     vx, vy = 0, 0
-    velocidade = 2
+    velocidade = 20
     uppress = False
     sair = False
     x = 0
@@ -209,7 +114,7 @@ def main(frames):
 
         if colisao(jogador, cano):
             sair = True
-            print(f'voce fez {int(pontos)} pontos')
+            print(f'Você fez {int(pontos)} pontos')
             frames = 60
             main(frames)
 
@@ -221,9 +126,3 @@ def main(frames):
 
 
 main(frames)
-
-pygame.time.delay(2000)  # Aguarde 2 segundos antes de sair
-pygame.quit()
-
-
-##TO DO: substituir player.png, obstaculo.png, musica_fundo.mp3 e som_colisao.wav 
